3582fcf6b31495e3f9cab486f5bd4c0e1b022a6a
charizmainyash <ikanisak.uc@gmail.com>
2024-03-11 11:03:07 +0530

    first commit

---
+++ controllers/alpha.js
@@ -0,0 +1,185 @@
+const USER=require("../models/user");
+const WORK=require("../models/work");
+const nodemailer = require('nodemailer');
+
+async function handleEnquiry(req,res)
+{
+    const body=req.body;
+     console.log("body",body);
+    if(!body ||
+        !body.name ||
+        !body.email ||
+        !body.inquiry)
+        {
+            return res.status(400).json({
+                error: "Data Not complete",
+            });
+        }
+    
+    const result=await USER.create({
+        name: body.name,
+        email: body.email,
+        inquiry: body.inquiry,
+    });
+
+    //email
+
+    
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: '0kanisak.uc@gmail.com',
+            pass: 'dnfb fcnz zvke uhed'
+        }
+        });
+    
+        var mailOptions = {
+        from: 'youremail@gmail.com',
+        to: body.email,
+        subject: 'Test mail from Alphazealmedia',
+        text: 'We have sucessfully received your enquiry, Our Team will get to you soon.'
+        };
+    
+        transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+        });
+    
+    
+        //email 2
+
+        var transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: '0kanisak.uc@gmail.com',
+                pass: 'dnfb fcnz zvke uhed'
+            }
+            });
+        
+            var mailOptions2 = {
+            from: 'youremail@gmail.com',
+            to: 'ikanisak.uc@gmail.com',
+            subject: 'Recived enquiry for Alphazealmedia',
+            text: body.name+' '+body.email+' '+body.inquiry
+            };
+        
+            transporter.sendMail(mailOptions2, function(error, info){
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email sent to admin : ' + info.response);
+            }
+            });
+
+
+ 
+        
+    if(result)
+    {
+        return res.json({ 
+            message: " New enquiry Created",
+            id :result._id,
+        });
+    }
+    else
+        return res.json({message:" enquiry not created"});
+}
+
+
+async function handleAdmin(req,res)
+{
+    const body=req.body;
+    // console.log(body);
+    if(!body ||
+        !body.email ||
+        !body.pass)
+        {
+            return res.status(400).json({
+                error: "Data Not complete",
+            });
+        }
+
+    const email=body.email;
+    const password=body.pass;
+    if(email=="admin@alphazeal.com" && password=="alphazeal@123")
+        return res.status(201).json({ "msg": "Admin Portel Access Permited" });
+    
+    return res.status(401).json({"msg":"Something is wrong"});
+}
+
+
+async function handleWork(req,res)
+{
+    const body=req.body;
+    console.log("body",body);
+    if(!body ||
+        !body.link ||
+        !body.title ||
+        !body.desc)
+        {
+            return res.status(401).json({
+                error: "Data Not complete",
+            });
+        }
+    
+    const result=await WORK.create({
+        link: body.link,
+        title: body.title,
+        description: body.desc,
+    });
+    if (result)
+        return res.status(201).json({ "msg": result }); 
+    return res.json({message:" WORK NOT ADDED"});
+}
+
+
+async function handleAllData(req, res) {
+    const data = await USER.find({});
+    console.log(data);
+    if (data)
+        return res.status(201).json({ "msg": data });
+
+    return res.status(401).json({ "msg": "something went wrong" });
+}
+
+async function handleAllWork(req, res) {
+    const data = await WORK.find({});
+    console.log(data);
+    if (data)
+        return res.status(201).json({ "msg": data });
+
+    return res.status(401).json({ "msg": "something went wrong" });
+}
+
+async function handleQueryResolve(req, res) {
+    const id = req.body.id;
+    console.log(id);
+    const result = await USER.deleteOne({ "_id": id });
+    if (result)
+        return res.status(201).json({ "msg": "deleted data" });
+    return res.status(401).json({ "msg": "something is wrong" });
+}
+
+async function removeWork(req, res) {
+    const id = req.body.id;
+    console.log(id);
+    const result = await WORK.deleteOne({ "_id": id });
+    if (result)
+        return res.status(201).json({ "msg": "deleted data" });
+    return res.status(401).json({ "msg": "something is wrong" });
+}
+
+
+module.exports={
+    handleEnquiry,
+    handleWork,
+    handleAdmin,
+    handleAllData,
+    handleAllWork,
+    handleQueryResolve,
+    removeWork,
+};
---
+++ index.js
@@ -0,0 +1,40 @@
+const express=require("express");
+const { connnectionFunction }=require("./config.js");
+const {handleEnquiry, handleWork, handleAdmin,handleAllData,handleAllWork,handleQueryResolve,removeWork }=require("./controllers/alpha.js");
+const app=express();
+const PORT=8009;
+const URL="mongodb+srv://aryashanish:9525633767@cluster0.j7tl5vl.mongodb.net/alphazeal?retryWrites=true&w=majority";
+const cors=require("cors");
+
+//Middleware
+app.use(cors(
+    {
+        origin: ["https://alpha-zeal.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+// MongoDB connection
+connnectionFunction(URL).then( ()=>{
+    console.log("DataBase Connected sucessfully");
+}).catch( (err)=> {
+    console.log(" Database connection failed : "+err);
+});
+
+
+app.post("/contact",handleEnquiry);
+app.post("/work",handleWork);
+app.post("/admin", handleAdmin);
+app.get("/getData", handleAllData);
+app.get("/getWork", handleAllWork);
+app.post("/resolveQuery", handleQueryResolve);
+app.post("/removeWork", removeWork);
+
+
+
+
+app.listen(PORT,()=> console.log(`Server Running at ${PORT}`));
